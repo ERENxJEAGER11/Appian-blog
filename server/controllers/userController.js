de9bfd051bca047c9878c4f2213af5de00786de6
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2024-01-21 21:52:05 +0530

    validateed users API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,12 @@ const pool = require('../database');
 
 exports.getAllUsers = async (req, res) => {
     try{
-        const query = 'SELECT * FROM "users" WHERE is_active=true';
-        const { rows } = await pool.query(query);
+        const is_active = req.body.is_active;
+        // only respnd with active users if is_active is not passed
+        const query = `SELECT * FROM "users" WHERE is_active = ${is_active === true || is_active === false ? $1 : 'true'}`;
+        const values = is_active === true || is_active === false ? [is_active] : [];
+
+        const { rows } = await pool.query(query,values);
         // Check if any users were retrieved
         if (rows.length === 0) {
             return res.status(404).json({ error: 'No users found' });
@@ -54,6 +58,38 @@ exports.deleteUserById = async (req, res) => {
         }
 
         res.json({ message: 'User deactivated  successfully' });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error: Something went wrong" });
+    }
+};
+
+exports.updateUser = async (req,res) => {
+    try {
+        const user_id = req.params.user_id;
+        const { username, password, full_name, email, bio } = req.body;
+
+        const query = `
+            UPDATE "users"
+            SET 
+                username = $1,
+                password = $2,
+                full_name = $3,
+                email = $4,
+                bio = $5
+            WHERE user_id = $6
+            RETURNING *;
+        `;
+
+        const values = [username, password, full_name, email, bio, user_id];
+        const { rows } = await pool.query(query, values);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'No user found with the provided user_id' });
+        }
+
+        res.json(rows[0]);
 
     } catch (err) {
         console.error(err);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 router.get('/users', userController.getAllUsers);
 router.get('/userById/:user_id',userController.getUserById);
 router.post('/deleteUserById/:user_id',userController.deleteUserById);
+router.post('/updateUser/:user_id',userController.updateUser);
 
 module.exports = router;
