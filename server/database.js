763a42b85139d75e1443680ecdd0b3b5368e2741
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2024-01-26 17:58:36 +0530

    feat:init: client code initialized_V_1

--- server/database.js
+++ server/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
-const pool  = new Pool({
-    host : "localhost",
-    port : 3001,
-    user : "postgres",
-    password : "@m@n$#@!k#",
-    database : "APPIAN_BLOG" 
-}); 
+const pool = new Pool({
+  host: 'localhost',
+  port: 3001,
+  user: 'postgres',
+  password: '@m@n$#@!k#',
+  database: 'APPIAN_BLOG',
+});
 
 module.exports = pool;
