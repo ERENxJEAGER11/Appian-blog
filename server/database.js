16763b408da8a96372df362fed039d028d2c5958
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2024-01-27 01:04:52 +0530

    feat: 💫 Added Login Structure_V-2

--- server/database.js
+++ server/database.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: 'localhost',
-  port: 3001,
+  port: 5432,
   user: 'postgres',
-  password: '@m@n$#@!k#',
-  database: 'APPIAN_BLOG',
+  password: 'Nuc@1234$*',
+  database: 'APPIAN BLOG',
 });
 
 module.exports = pool;
