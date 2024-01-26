2a18608ed832503e5ad78dde363bf61d9f394083
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2024-01-26 19:27:18 +0530

    feat: 🛣️ added auth routing_V-1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,26 @@
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+import LoginForm from './components/Auth/LoginForm';
+import RegisterForm from './components/Auth/RegisterForm';
+import Home from './components/Layout/Home';
+import Error from './components/Layout/Error';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <LoginForm /> },
+      { path: '/register', element: <RegisterForm /> },
+    ],
+  },
+]);
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
