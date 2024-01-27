d4481b70f696d07300b0a7ee52be1ca40fb34f11
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2024-01-27 19:26:12 +0530

    Feat : Introduces Category Routess)_V-1

--- server/routes/categoriesRoutes.js
+++ server/routes/categoriesRoutes.js
@@ -5,5 +5,6 @@ const categoriesController = require("../controllers/categoriesController");
 router.get('/categories', categoriesController.getAllCategories);
 router.get('/categoryById', categoriesController.getCategoryById);
 router.post('/deleteCategoryById', categoriesController.deleteCategoryById);
+router.post('/createCategory', categoriesController.createCategory);
 
 module.exports = router;
