e034b237e5c8738b4ae64048ad63355b8ff65c86
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2024-01-27 14:41:12 +0530

    feat: 💫 Added Login UI_V-2

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-light-blue': '#dbeafe',
+        'custom-medium-blue': '#68d8e8',
+        'custom-dark-blue': '#312e81',
+        'custom-darker-blue': '#1e1b4b',
+      },
+    },
     fontFamily: {
       display: ['Poppins'],
     },
