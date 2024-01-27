c232e5dac7e752d9da03a59815603c310ffc8ceb
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2024-01-27 23:05:10 +0530

    feat: Added Shadcn and Home page structure

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('userDetails') ? true : false
+  );
+  const handleLogout = () => {
+    localStorage.removeItem('userDetails');
+    setIsLoggedIn(false);
+  };
+  return (
+    <nav className='flex justify-between'>
+      <h1>CodXMan</h1>
+      {isLoggedIn ? (
+        <>
+          <Button onClick={handleLogout}>Log out</Button>
+        </>
+      ) : (
+        <>
+          <Button variant='link' to='/login'>
+            <Link to='/login'>Log In</Link>
+          </Button>
+        </>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
