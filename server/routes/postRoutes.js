ced822852793a48d8bbf9b2e43a190aabc4ed058
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2024-02-01 21:17:12 +0530

    route added

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,6 +4,7 @@ const postController = require("../controllers/postController");
 
 router.get('/getPostById', postController.getPostByPostId);
 router.post('/deletePostById', postController.deletePostById);
+router.post('/createPost', postController.createPost)
 
 module.exports = router;
 
